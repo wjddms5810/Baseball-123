00ea981c245dcabdb5be23998ae1ae641e01c11e
BaeJoungEun <wjddms5810@naver.com>
2025-07-31 13:53:56 +0900

    [refactoring] 28

--- test_game.py
+++ test_game.py
@@ -1,8 +1,10 @@
 import pytest
 from game import Game
 
+@pytest.fixture
+def game():
+    return Game()
 
-def test_exception_when_input_is_none():
-    game = Game()
+def test_exception_when_input_is_none(game):
     with pytest.raises(TypeError):
         game.guess(None)
